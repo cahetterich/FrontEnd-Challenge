c978b1f3587d1af45b043dc9c44c1ed93a4be1ec
Carla H <hetterichcarla@gmail.com>
2024-09-29 18:55:17 +0200

    video pitch

--- assets/js/perfil.js
+++ assets/js/perfil.js
@@ -1,29 +1,29 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const token = localStorage.getItem('token');
-    if (!token) {
-        window.location.href = './login.html';
-        return;
-    }
+// document.addEventListener('DOMContentLoaded', async () => {
+//     const token = localStorage.getItem('token');
+//     if (!token) {
+//         window.location.href = './login.html';
+//         return;
+//     }
 
-    try {
-        const response = await fetch('http://localhost:8080/api/user/profile', {
-            method: 'GET',
-            headers: {
-                'Authorization': `Bearer ${token}`
-            }
-        });
+//     try {
+//         const response = await fetch('http://localhost:8080/api/user/profile', {
+//             method: 'GET',
+//             headers: {
+//                 'Authorization': `Bearer ${token}`
+//             }
+//         });
 
-        if (response.ok) {
-            const user = await response.json();
-            // Atualize os elementos HTML com os dados do usuário
-            document.querySelector('.profile-name').textContent = `${user.firstName} ${user.lastName}`;
-            document.querySelector('.profile-about p').textContent = user.about || 'Sem informações adicionais.';
-            // Atualize outras seções conforme necessário
-        } else {
-            const error = await response.text();
-            alert(error);
-        }
-    } catch (error) {
-        console.error('Erro:', error);
-    }
-});
+//         if (response.ok) {
+//             const user = await response.json();
+//             // Atualize os elementos HTML com os dados do usuário
+//             document.querySelector('.profile-name').textContent = `${user.firstName} ${user.lastName}`;
+//             document.querySelector('.profile-about p').textContent = user.about || 'Sem informações adicionais.';
+//             // Atualize outras seções conforme necessário
+//         } else {
+//             const error = await response.text();
+//             alert(error);
+//         }
+//     } catch (error) {
+//         console.error('Erro:', error);
+//     }
+// });
